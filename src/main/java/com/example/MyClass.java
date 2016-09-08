1f2f31f78802385aefb1dcb0a1389aafc2f13d26
joros96 <joros96@gmail.com>
2016-09-08 12:29:01 -0500

    initializeVariablesJonathanO

---
+++ src/main/java/com/example/MyClass.java
@@ -0,0 +1,30 @@
+// Program: Initialize Variables
+// Programmer: Jonathan Oros
+// Purpose:
+// Date: 09/06/2016
+package com.example;
+
+public class MyClass
+{
+    public static void main(String[] args)
+    {
+        byte num1 = 50;
+        short num2 = 56;
+        int num3 = 22542;
+        long num4 = 5443772;
+        float num5 = 1.2f;
+        double num6 = 5.154343;
+        boolean user = true;
+        char letter1 = 'A';
+
+        System.out.println("Byte number is: " + num1);
+        System.out.println("Short number is: " + num2);
+        System.out.println("Integer number is: " + num3);
+        System.out.println("Long number is: " + num4);
+        System.out.println("Float number is: " + num5);
+        System.out.println("Double number is: " + num6);
+        System.out.println("Boolean is: " + user);
+        System.out.println("Character letter is: " + letter1);
+
+    }
+}
